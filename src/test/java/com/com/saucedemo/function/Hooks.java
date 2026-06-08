b9d8411e0a8e4998b0f04b2ffebfa722d0d8ab4d
Jhonny Poloche <jhonnypupa1310@gmail.com>
2026-06-08 15:28:24 -0500

    feat(Runner): Configure TestRunner with glue and plugin, and update Hooks with ChromeOptions

--- src/test/java/com/com/saucedemo/function/Hooks.java
+++ src/test/java/com/com/saucedemo/function/Hooks.java
@@ -1,22 +1,38 @@
 package com.com.saucedemo.function;
 
+import com.com.saucedemo.utilities.ReportUtils;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Hooks extends TextContext{
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hooks extends TextContext {
 
     @Before
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver","src/test/resources/configuration/chromedriver.exe");
-        driver = new ChromeDriver();
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/configuration/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-features=PasswordLeakDetection");
+        options.addArguments("--disable-web-security");
+        options.addArguments("--allow-running-insecure-content");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
     }
 
     @After
-    public void getDown(){
+    public void getDown() {
         driver.quit();
+        ReportUtils.getInstance().flush();
     }
-
 }
