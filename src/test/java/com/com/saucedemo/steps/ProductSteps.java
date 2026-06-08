e011c35f88e7603d889ec559dbde207d0d7b948c
Jhonny Poloche <jhonnypupa1310@gmail.com>
2026-06-08 16:00:21 -0500

    feat(add): Add steps

---
+++ src/test/java/com/com/saucedemo/steps/ProductSteps.java
@@ -0,0 +1,45 @@
+package com.com.saucedemo.steps;
+
+import com.com.saucedemo.function.TextContext;
+import com.com.saucedemo.page.ProductDetailPage;
+import com.com.saucedemo.page.ProductsPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProductSteps {
+
+    private ProductsPage productsPage() {
+        return new ProductsPage(TextContext.driver);
+    }
+
+    @When("I select {string} from the sort dropdown")
+    public void iSelectFromTheSortDropdown(String sortOption) {
+        productsPage().selectSortOption(sortOption);
+    }
+
+    @And("I click on the first product")
+    public void iClickOnTheFirstProduct() {
+        productsPage().clickFirstProduct();
+    }
+
+    @Then("I should see the product detail page")
+    public void iShouldSeeTheProductDetailPage() {
+        new ProductDetailPage(TextContext.driver).verifyProductDetailPageLoaded();
+    }
+
+    @When("I click {string} on the first product")
+    public void iClickOnTheFirstProductAction(String action) {
+        productsPage().clickAddToCartOnFirstProduct();
+    }
+
+    @Then("I should see the cart badge showing {string}")
+    public void iShouldSeeTheCartBadgeShowing(String count) {
+        productsPage().verifyCartBadge(count);
+    }
+
+    @And("the product should be in my cart")
+    public void theProductShouldBeInMyCart() {
+        productsPage().navigateToCart();
+    }
+}
